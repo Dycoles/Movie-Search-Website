4302d3dd6c1439d0ad09d4d805d8a876e8f9b0f1
Dycoles <134562267+Dycoles@users.noreply.github.com>
2026-02-02 00:31:45 -0700

    Create website and hook up database

---
+++ movieSite/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  const count: number = 42;
+  return (
+    <>
+      <div className="p-4">
+        {count}
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import { db } from "./db.js";
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("New API running");
+});
+
+app.get("/movies", async (req, res) => {
+  try {
+    const [rows] = await db.query("SELECT * FROM movies");
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Database error" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
